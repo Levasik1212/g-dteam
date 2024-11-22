1903d38e98c609e57837348f52875c6b128a9d71
Levasik1212 <grisha.shara23@gmail.com>
2024-11-22 22:14:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Прокрутка до якорных ссылок плавно
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
